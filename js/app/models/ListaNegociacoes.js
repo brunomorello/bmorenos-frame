f90e5d53919e86ba85982327c09cfca44a3aacb0
Bruno Moreno <brunomorello7@gmail.com>
2018-03-30 13:23:17 -0300

    Created a new Model to list Negociacoes

---
+++ js/app/models/ListaNegociacoes.js
@@ -0,0 +1,23 @@
+class ListaNegociacoes {
+	
+	constructor () {
+
+		this._negociacoes = [];
+
+	}
+
+	adicionar(negociacao) {
+
+		this._negociacoes.push(negociacao);
+
+	}
+
+
+	get negociacoes() {
+
+		return [].concat(this._negociacoes);
+
+	}
+
+
+}
