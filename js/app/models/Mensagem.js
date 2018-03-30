9c252c9f2faac2b91a051c484286f42e15f99fe1
Bruno Moreno <brunomorello7@gmail.com>
2018-03-30 16:32:10 -0300

    Created a Message Model

---
+++ js/app/models/Mensagem.js
@@ -0,0 +1,17 @@
+class Mensagem {
+	
+	constructor(texto = "") {
+
+		this._mensagem = texto;
+
+	}
+
+	get mensagem() {
+		return this._mensagem;
+	}
+
+	set mensagem(texto) {
+		this._mensagem = texto;
+	} 
+
+}
