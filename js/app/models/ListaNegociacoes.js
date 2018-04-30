e959b47d11857ec7302e3b09dee92dc01c2af42e
Bruno Moreno <brunomorello7@gmail.com>
2018-04-30 11:41:50 -0300

    Moved the volume calc amount to the model

--- js/app/models/ListaNegociacoes.js
+++ js/app/models/ListaNegociacoes.js
@@ -11,15 +11,19 @@ class ListaNegociacoes {
 	}
 
 
-	get negociacoes() {
-
-		return [].concat(this._negociacoes);
-	}
 
 	esvaziar() {
 
 		this._negociacoes = [];
 	}
 
+	get negociacoes() {
+
+		return [].concat(this._negociacoes);
+	}
+
+	get volumeTotal() {
+		return this._negociacoes.reduce((total, n) => total + n.volume, 0.0);
+	}
 
 }
