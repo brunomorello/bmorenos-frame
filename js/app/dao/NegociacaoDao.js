f84cf5ebe12983c8dec619b1b055be1dcbf731e4
Bruno Moreno <bmorenos@br.ibm.com>
2019-05-06 22:31:16 -0300

    Function to get all records stored locally on Indexed DB

--- js/app/dao/NegociacaoDao.js
+++ js/app/dao/NegociacaoDao.js
@@ -29,4 +29,45 @@ class NegociacaoDao {
 
     }
 
+    getLocalNegotiations() {
+
+        return new Promise((resolve, reject) => {
+
+            let request = this._connection
+                .transaction([this._store], 'readwrite')
+                .objectStore(this._store)
+                .openCursor();
+
+            let negotiationsList = [];
+
+            request.onsuccess = e => {
+
+                let currentPosition = e.target.result;
+
+                if(currentPosition) {
+                    // storing current negotiation
+                    let negotiation = currentPosition.value;
+
+                    // pushing it to array
+                    negotiationsList.push(new Negociacao(negotiation._data, negotiation._quantidade, negotiation._valor));
+
+                    // moving cursor
+                    currentPosition.continue();
+
+                } else {
+                    // when finishes read list, resolve this Promise
+                    resolve(negotiationsList);
+                }
+                
+            };
+            
+
+            request.onerror = e => {
+                reject(`Error to Get Local Negotiations ${e.target.error}`);
+            };
+
+        })
+
+    }
+
 }
