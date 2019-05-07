a1076f637dc38a2078016e19cdea0d4537100a6e
Bruno Moreno <bmorenos@br.ibm.com>
2019-05-06 21:32:46 -0300

    Imported DAO Class for Negotiations

---
+++ js/app/dao/NegociacaoDao.js
@@ -0,0 +1,32 @@
+class NegociacaoDao {
+
+    constructor(connection) {
+        
+        this._connection = connection;
+        this._store = 'negotiations';
+
+    }
+
+    add(negotiation) {
+
+        return new Promise((resolve, reject) => {
+
+            let request = this._connection
+                .transaction([this._store], 'readwrite')
+                .objectStore(this._store)
+                .store.add(negotiation);
+            
+            request.onsuccess = e => {
+                //console.log(`success to insert object`);
+                resolve();
+            };
+
+            request.onerror = e => {
+                reject(`Error to Insert Object ${e.target.error}`);
+            }
+
+        });
+
+    }
+
+}
