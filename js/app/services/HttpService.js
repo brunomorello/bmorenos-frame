cc88fa43b67e73f0cb5e6b3674000c1522c66d34
Bruno Moreno <bmorenos@br.ibm.com>
2019-05-13 22:03:21 -0300

    Implemented post function

--- js/app/services/HttpService.js
+++ js/app/services/HttpService.js
@@ -11,4 +11,14 @@ class HttpService {
 			.then(res => this._handleErrors(res))
 			.then(res => res.json());
 	}
+
+	post(url, data) {
+
+		return fetch(url, {
+			headers: { 'Content-type' : 'application/json' },
+			method: 'post',
+			body: JSON.stringify(data)
+		})
+		.then(res => this._handleErrors(res));
+	}
 }
